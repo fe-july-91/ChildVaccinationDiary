649614a4cf0617fcaa8243e8fecaa2fcf59037b3
Yana Stepanova <yanna.stepanova@gmail.com>
2024-10-18 08:32:04 +0300

    added postgres and changed yaml-files for validation entities with db

--- src/main/java/com/team/project/ChildVaccinationDiary/model/User.java
+++ src/main/java/com/team/project/ChildVaccinationDiary/model/User.java
@@ -31,6 +31,7 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false)
