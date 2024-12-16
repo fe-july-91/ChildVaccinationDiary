b7f8fd4a2564f1751c4b1c2905582aff5fa7c7f1
Yana Stepanova <yanna.stepanova@gmail.com>
2024-12-16 12:12:00 +0200

    try to fix "Could not deserialize ATN with version 4 (expected 3)."

--- src/main/java/team/project/ChildVaccinationDiaryApplication.java
+++ src/main/java/team/project/ChildVaccinationDiaryApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackages = "team.project.repository")
 public class ChildVaccinationDiaryApplication {
 
     public static void main(String[] args) {
--- src/main/java/team/project/repository/vaccine/VaccineRepository.java
+++ src/main/java/team/project/repository/vaccine/VaccineRepository.java
@@ -6,10 +6,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import team.project.model.Vaccine;
 
-@Repository
 public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
     List<Vaccine> findAllByChildId(Long childId);
 
