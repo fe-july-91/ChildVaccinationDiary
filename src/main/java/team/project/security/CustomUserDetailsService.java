573a3c41ceb47c21f5b053c14ca2274ac1b574ff
Yana Stepanova <yanna.stepanova@gmail.com>
2025-04-24 18:57:54 +0300

    fixed authentication

--- src/main/java/team/project/security/CustomUserDetailsService.java
+++ src/main/java/team/project/security/CustomUserDetailsService.java
@@ -3,8 +3,8 @@ package team.project.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import team.project.exception.EntityNotFoundCustomException;
 import team.project.repository.UserRepository;
 
 @Service
@@ -13,8 +13,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepo;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws EntityNotFoundCustomException {
-        return userRepo.findByEmail(email).orElseThrow(() -> new EntityNotFoundCustomException(
-                String.format("Не вдається знайти користувача за електронною адресою %s", email)));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(
+                String.format("Не вдається знайти користувача за електронною адресою %s", username)));
     }
 }
